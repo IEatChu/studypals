01a7cdfc1a76efe9f3c2e5fe010efce90e8dd9a7
Roger <huaquan@hawaii.edu>
2025-04-17 15:00:43 -1000

    Update page.tsx

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h1>About Study Buddy</h1>
+    <p>
+      Study Buddy is an app designed to help UHM ICS students organize face-to-face study groups in ICSpace.
+    </p>
+  </div>
+);
+
+export default About;
