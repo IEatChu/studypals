31f130eda2d88053652f1f756c7071e0261b9630
Roger <huaquan@hawaii.edu>
2025-04-17 14:58:18 -1000

    added about and contact

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+// src/app/contact/page.tsx
+import React from 'react';
+
+const Contact = () => (
+  <div>
+    <h1>Contact Us</h1>
+    <p>If you have any questions or need support, feel free to reach out!</p>
+    <p>Email: support@studybuddy.com</p>
+  </div>
+);
+
+export default Contact;
